e1f4787921835999770d5ea0acd03cc870e70111
Zick108 <zickkkotun@gmail.com>
2020-10-19 16:36:43 +0630

    Pop-up box for Full-screen view 19-10-2020-16:47

---
+++ profile/js/script.js
@@ -0,0 +1,31 @@
+/* Chrome, Safari and Opera syntax */
+// function getConfirmation() {
+//     document.getElementById("req").style="visibility:visible;";
+// }
+
+function openFullscreen() {
+  var elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+  document.getElementById("req").style="visibility:hidden;z-index:8;";
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+  document.getElementById("req").style="visibility:hidden;z-index:8;";
+}
